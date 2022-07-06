35f62ce96de07f86e68e56cab4d53607fb89bdc3
quannv02 <2059033@itec.hcmus.edu.vn>
2022-07-07 00:33:43 +0700

    add order.php

---
+++ views/order.php
@@ -0,0 +1,28 @@
+<?php
+include "includes/head.php"
+?>
+
+<link rel="stylesheet" href="includes/css/create.css">
+
+<div class="container-sm mt-3">
+    <h2>Order Information</h2>
+    <p>Tracking number: </p>
+    <p>Time created: </p>
+    <p>Shipping fee: </p>
+    <hr>
+    <h4>Sender: </h4>
+    <p>Name: </p>
+    <p>Address: </p>
+    <p>Mobile number: </p>
+    <hr>
+    <h4>Recipient: </h4>
+    <p>Name: </p>
+    <p>Address: </p>
+    <p>Mobile number: </p>
+    <hr class="mb-0">
+    <a href="user.php" class="btn mt-2 mb-3">Back</a>
+</div>
+
+<?php
+include "includes/footer.php"
+?>
