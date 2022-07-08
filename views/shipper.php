c9b2112d93da0f00b4b7d67f22f5769aa382c53a
quannv02 <2059033@itec.hcmus.edu.vn>
2022-07-08 18:11:56 +0700

    update shipper page

--- views/shipper.php
+++ views/shipper.php
@@ -1,3 +1,61 @@
-<?php
+<link rel="stylesheet" href="<?= ROOT ?>/public/css/shipper.css"></link>
 
-echo "Shipper page";
+<div class="orders jumbotron bg-white mx-4 my-3 p-5 rounded-0">
+    <h3 class="mb-3">Available Orders</h3>
+    <div class="list-group">
+        <div class="order p-3 d-flex flex-wrap justify-content-between">
+            <a href="" class="order-info">
+                <p class="m-0 font-weight-bold"><span class="font-weight-normal">From: </span> Dai Hoc Khoa Hoc Tu Nhien An Duong Vuong</p>
+                <p class="m-0 font-weight-bold"><span class="font-weight-normal">To: </span>Dai Hoc Xa Hoi Nhan Van Thu Duc</p>
+                <p class="m-0">(Shipping Fee)</p>
+            </a>
+            <button class="btn text-white" type="submit" action="">Pick Up</button>
+        </div>
+        <div class="order p-3 d-flex flex-wrap justify-content-between">
+            <a href="" class="order-info">
+                <p class="m-0 font-weight-bold"><span class="font-weight-normal">From: </span> Dai Hoc Khoa Hoc Tu Nhien An Duong Vuong</p>
+                <p class="m-0 font-weight-bold"><span class="font-weight-normal">To: </span>Dai Hoc Xa Hoi Nhan Van Thu Duc</p>
+                <p class="m-0">(Shipping Fee)</p>
+            </a>
+            <button class="btn text-white" type="submit" action="">Pick Up</button>
+        </div>
+        <div class="order p-3 d-flex flex-wrap justify-content-between">
+            <a href="" class="order-info">
+                <p class="m-0 font-weight-bold"><span class="font-weight-normal">From: </span> Dai Hoc Khoa Hoc Tu Nhien An Duong Vuong</p>
+                <p class="m-0 font-weight-bold"><span class="font-weight-normal">To: </span>Dai Hoc Xa Hoi Nhan Van Thu Duc</p>
+                <p class="m-0">(Shipping Fee)</p>
+            </a>
+            <button class="btn text-white" type="submit" action="">Pick Up</button>
+        </div>
+        
+    </div>
+</div>
+
+<div class="orders jumbotron bg-white mx-4 my-3 p-5 rounded-0">
+    <h3 class="mb-3">Progressing Orders</h3>
+    <div class="list-group">
+        <div class="order p-3 d-flex flex-wrap justify-content-between">
+            <a href="" class="order-info">
+                <p class="m-0 font-weight-bold"><span class="font-weight-normal">From: </span> Dai Hoc Khoa Hoc Tu Nhien An Duong Vuong</p>
+                <p class="m-0 font-weight-bold"><span class="font-weight-normal">To: </span>Dai Hoc Xa Hoi Nhan Van Thu Duc</p>
+                <p class="m-0">(Shipping Fee)</p>
+            </a>
+            <button class="btn text-white" type="submit" action="">Done</button>
+        </div>
+        
+    </div>
+</div>
+
+<div class="orders jumbotron bg-white mx-4 my-3 p-5 rounded-0">
+    <h3 class="mb-3">Completed Orders</h3>
+    <div class="list-group">
+        <div class="order p-3 d-flex flex-wrap justify-content-between">
+            <a href="" class="rder-info">
+                <p class="m-0 font-weight-bold"><span class="font-weight-normal">From: </span> Dai Hoc Khoa Hoc Tu Nhien An Duong Vuong</p>
+                <p class="m-0 font-weight-bold"><span class="font-weight-normal">To: </span>Dai Hoc Xa Hoi Nhan Van Thu Duc</p>
+                <p class="m-0">(Shipping Fee)</p>
+            </a>
+        </div>
+        
+    </div>
+</div>
