db6b807c3bc776b63122b483d5bd072fecd07923
hiennguyn <2059014@itec.hcmus.edu.vn>
2022-07-09 18:54:15 +0700

    order update

---
+++ controllers/OrdersController.php
@@ -0,0 +1,39 @@
+<?php
+
+class OrdersController extends Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getCreateOrder() {
+        include "views/create.php";
+    }
+
+    // controller methods
+    public function getOrder($id) {
+        $order = new Order($this->conn);
+        if($order->fetchOrder($id)->success()) {
+          $order = $order->getOrder();
+          include "views/order.php";
+        } else {
+          include "views/order.php";
+          //include "views/_404.php";
+        }
+    }
+
+    public function create() {
+        $order = new Order($this->conn);
+        if($order->validateOrder($this->req)->success()) {
+           if($order->createNewOrder()->success()) {
+            header("Location: " . ROOT . "order/" . $order->order_id);
+            $successNoti = "Successfully placed the order!";
+            include "views/order.php";
+           }
+        } else {
+            $errors = $order->errors;
+            include "views/create.php";
+        }
+    }
+}
