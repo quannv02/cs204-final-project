ddd06885ac22199a4d711380dd18a395c8d2f4fa
quannv02 <2059033@itec.hcmus.edu.vn>
2022-07-10 16:23:21 +0700

    update home

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,23 @@
+window.addEventListener("load", function() {
+    document.querySelector(".hero-title").classList.add("appear");
+    document.querySelector(".hero-btn").classList.add("appear");
+});
+
 window.addEventListener('scroll', function () {
     let navbar = document.querySelector('nav');
-    let windowPosition = window.scrollY > 0;
-    navbar.classList.toggle('after-scroll', windowPosition);
+    if (window.innerWidth > 1125) {
+        let windowPosition = (window.scrollY > 175 && window.scrollY < 565) || window.scrollY > 1200;
+        navbar.classList.toggle('after-scroll', windowPosition);
+    } else {
+        let windowPosition = (window.scrollY > 175 && window.scrollY < 650) || window.scrollY > 1250;
+        navbar.classList.toggle('after-scroll', windowPosition);
+    }
+})
+
+window.addEventListener('scroll', function () {
+    var elem = document.querySelector('.believe');
+    var distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
+    if (distInView < 0) {
+        elem.classList.add("appear");
+    }
 })
--- views/home.php
+++ views/home.php
@@ -5,21 +5,43 @@ include "includes/head-home.php"
 <script type="text/javascript" src="<?=ROOT?>public/js/home.js"></script>
 
 <div class="hero">
-    <div class="container p-0">
-        <div class="hero-title display-1 font-weight-bolder text-white">
-            Fast and <br>Reliable Shipping
+    <div class="container hero-content p-0">
+        <div class="container p-0 m-0">
+            <div class="hero-title display-1 font-weight-bolder text-white">
+                Fast and <br>Reliable Shipping
+            </div>
+            <?php if($_SESSION['logged_in'] == false): ?>
+                <a href="<?=ROOT?>signup" style="width: 160px;">
+                    <div class="hero-btn"><h3 class="m-0 text-center">Join Us</h3></div>
+                </a>
+            <?php elseif($_SESSION['user_role'] == 2):?>
+                <a href="<?=ROOT?>create" style="width: 225px;">
+                    <div class="hero-btn"><h3 class="m-0 text-center">Create Order</h3></div>
+                </a>
+            <?php endif;?>
         </div>
-        <?php if($_SESSION['logged_in'] == false): ?>
-            <a href="<?=ROOT?>signup">
-                <div class="hero-btn" style="width: 150px;"><h3 class="m-0 text-center">Join Us</h3></div>
-            </a>
-        <?php elseif($_SESSION['user_role'] == 2):?>
-            <a href="">
-                <div class="hero-btn" style="width: 225px;"><h3 class="m-0 text-center">Create Order</h3></div>
-            </a>
-        <?php endif;?>
+        
+        <div class="believe">
+            <h1 class="display-5 font-weight-bold">What we believe</h1>
+            <p class="lead">
+            We believe there's always a smarter and better way of doing things, which means we're constantly innovating and pushing the boundaries of how an agency can help its clients make better delivery.        
+            </p>
+        </div>
+
+        <div class="tracking">
+            <h1 class="display-5 font-weight-bold mb-3">Track your parcel</h1>
+            <div class="d-flex justify-content-between flex-wrap container p-0 m-0">
+                <div class="wrap-input100 validate-input m-0">
+                    <input class="input100" type="text" name="tracking-number" placeholder="Tracking number">
+                    <span class="focus-input100"></span>
+                </div>
+                <button class="btn btn-outline-light text-white">Search</button>
+            </div>
+            
+        </div>
     </div>
 </div>
+
 
 <?php
 include "includes/footer.php"
